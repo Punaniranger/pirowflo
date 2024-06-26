0219ac2c6612ee9d2e3b25be242c791d47a47409
TbsJah <34193151+TbsJah@users.noreply.github.com>
2024-06-26 10:23:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ source piroflo/bin/activate
 pip3 install pyserial
 pip3 install PyGObject
 pip3 install dbus-python
-pip3 install numpy
+# pip3 install numpy
 pip3 install pyusb
 pip3 install gatt 
 pip3 install supervisor 
