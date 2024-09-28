074742a07d95e20252dcc8558414ee08d9dac4a2
Punaniranger <e.amico@web.de>
2024-09-28 19:53:34 +0200

    Update driver.sh

--- driver.sh
+++ driver.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+sudo apt-get install -y python3 python3-gi python3-dev python3-gi-cairo gir1.2-gtk-3.0 python3-pip
+sudo apt-get install -y libatlas-base-dev libglib2.0-dev libgirepository1.0-dev libcairo2-dev zlib1g-dev
+sudo apt-get install -y libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff6
+sudo apt-get install -y build-essential libdbus-glib-1-dev
+sudo apt-get install -y git virtualenv
+
 python3 -m venv pirowflo
 source pirowflo/bin/activate
 
@@ -11,3 +17,4 @@ pip3 install gatt
 pip3 install supervisor
 pip3 install luma.oled
 pip3 install spidev
+
